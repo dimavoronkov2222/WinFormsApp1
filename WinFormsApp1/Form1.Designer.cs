651bc12ffa568731c054552a677a9b19c8f25d69
Дмитро Воронков <dimavoronkov2222@hotmail.com>
2023-10-18 18:35:25 +0200

    add lines

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -28,12 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            progressBar1 = new ProgressBar();
+            openFileDialog1 = new OpenFileDialog();
+            label1 = new Label();
+            button1 = new Button();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(12, 316);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(776, 103);
+            progressBar1.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 284);
+            label1.Name = "label1";
+            label1.Size = new Size(17, 20);
+            label1.TabIndex = 1;
+            label1.Text = "0";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(328, 142);
+            button1.Name = "button1";
+            button1.Size = new Size(125, 52);
+            button1.TabIndex = 2;
+            button1.Text = "Open file";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(747, 284);
+            label3.Name = "label3";
+            label3.Size = new Size(41, 20);
+            label3.TabIndex = 4;
+            label3.Text = "2000";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(progressBar1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Files Reader";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ProgressBar progressBar1;
+        private OpenFileDialog openFileDialog1;
+        private Label label1;
+        private Button button1;
+        private Label label3;
     }
 }
